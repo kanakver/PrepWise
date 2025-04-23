dac23be3b4516583a2f1725193384629e8492e5f
Kanak verma <kanakv008@gmail.com>
2025-04-24 03:58:11 +0530

    fixed problem

--- firebase/admin.ts
+++ firebase/admin.ts
@@ -7,12 +7,19 @@ function initFirebaseAdmin() {
   const apps = getApps();
 
   if (!apps.length) {
+    const privateKey = process.env.FIREBASE_PRIVATE_KEY
+      ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n")
+      : undefined;
+
+    if (!privateKey) {
+      throw new Error("FIREBASE_PRIVATE_KEY environment variable is not set");
+    }
+
     initializeApp({
       credential: cert({
         projectId: process.env.FIREBASE_PROJECT_ID,
         clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-        // Replace newlines in the private key
-        privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+        privateKey: privateKey,
       }),
     });
   }
